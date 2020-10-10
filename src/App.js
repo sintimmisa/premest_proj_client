599b147222d3bdeb54193fe3545770f22c91c4eb
SIM <sintim.misa@gmail.com>
2020-10-10 13:18:07 +0000

    FEA: Added showNavabar Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import './App.css';
+import Header from './components/Header/Header';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-        Welcome to VegOnline Store
-      
-    </div>
+    <Router>
+      <Header/>
+    </Router>
   );
 }
 
